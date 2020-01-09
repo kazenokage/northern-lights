8341f72c61da921620a0fac513d633dd76233a74
Tommi Nikkanen <tommi.nikkanen@saunalahti.fi>
2020-01-09 21:52:55 +0200

    Demo 0.1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:northern_lights/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(NorthernApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
